e1e4b9f0fc7cb3b8b8d689ee6797546db0703ba3
Yoshiya04 <major.yoshiya@gmail.com>
2021-10-21 00:06:40 +0900

    change1-q1 (2)

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -1,14 +1,15 @@
 // 練習問題：神戸市のマーク
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(400, 200);
   background(255);
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
+  stroke(22, 131, 46);
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  arc(70, 75+25, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
+  arc(100 - 25, 75+25, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
   // BLANK[1]
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
@@ -16,4 +17,14 @@ function setup(){
   const blue = color(14, 47, 146);
   const green = color(22, 131, 46);
   const gray = color(77);
+
+  fill(blue);
+  noStroke();
+  ellipse(300,100,100,100);  //青い円
+  fill(255);
+  ellipse(294,96,91,91);　//白い円
+  fill(green);
+  ellipse(294,95,82,82);　//緑の円
+  fill(255);
+  ellipse(292,100,77,77);　//白い円
 }
