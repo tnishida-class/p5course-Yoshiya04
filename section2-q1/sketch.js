8e0c1ec2f05e220d4962ce6cb98ac85e85208edd
Yoshiya04 <major.yoshiya@gmail.com>
2021-10-21 00:09:05 +0900

    change2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
-// 小手調べ
+// 小手調べ //まずは一個ずつ円書いてみる
 function setup(){
+noFill();
+
   for(let i = 0; i < 10; i++){
+    let r=i*10+10;
+    if(i<5){   //iが５より小さいとき青
+      stroke(0,0,225);
+    }
+    else{　　　//iが5以上のとき赤
+      stroke(255,0,0);
+    }
+    ellipse(50,50,r);
+
     // BLANK[1]
   }
 }
