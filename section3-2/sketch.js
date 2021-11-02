3d252d970e779b07a643a4e4a03ef3f48145fef4
Yoshiya04 <major.yoshiya@gmail.com>
2021-11-02 11:31:22 +0900

    change3-2(2)

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -15,23 +15,27 @@ function draw(){
   ellipse(x, y, 20);
   if(keyIsDown(LEFT_ARROW)){ x -= vx; }
   if(keyIsDown(RIGHT_ARROW)){ x += vx; }
-  if(keyIsDown(" ".charCodeAt(0))){
-    y -= vy;
-    vy += g;
-    vy = constrain(vy+g, -vyMax, vyMax);
+  if(y >= height/2-20 && keyIsDown(" ".charCodeAt(0))){
+    // y >= height-20は空中で2回以上ジャンプできないようにする
+    vy = -20;
   }
-      if(x < 0 || x > width){ vx = -1 * vx; }
-      if(y < 0 || y > height){ vy = -1 * vy; }
-      x = constrain(x, 0, width);
-      y = constrain(y, 0, height);
-}
-function windowResized(){
+    y += vy;
+    vy = constrain(vy+g, -vyMax, vyMax);
+
+//   if(x < 0 || x > width){ vx = -1 * vx; }勝手に跳ね返らない
+  if(y > height){ vy = 0; }　
+  x = constrain(x, 0, width);
+  y = constrain(y, 0, height / 2);
+   //height/2をheigt-10にしたら一番下にボールが来る
+   // 一番下にボールを置く場合は、18のheight/2-20をheight-20に変更する
+
+  function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
+ }
 }
 
 
 
-//
 // // テキスト「アニメーションの基本」 元の教材
 // let x, y, vx, vy;
 // const g = 1; // 重力加速度
