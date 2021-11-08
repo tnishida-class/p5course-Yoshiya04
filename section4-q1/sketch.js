4c175d0ebf5897906254105ae992a87fa5af0a86
Yoshiya04 <major.yoshiya@gmail.com>
2021-11-09 00:07:52 +0900

    change4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -7,7 +7,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
 
   // 横線を引く
@@ -20,5 +20,11 @@ function setup(){
   let px, py; // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
     // BLANK[1]
+  ellipse(i*dx+20,height-(height*scores[i]/100),8);
+  line(i*dx+20,height-(height*scores[i]/100),(i+1)*dx+20,height-(height*scores[i+1]/100));
+  text(scores[i].toPrecision(3),i*dx+20, height-(height*scores[i]/100));
+
+  // px=scores[i-1];
+  // py=height-(height*scores[i-1]/100) 
   }
 }
