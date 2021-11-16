81115264ff20126f4f11b21fb93db733240605a7
Yoshiya04 <major.yoshiya@gmail.com>
2021-11-16 09:24:47 +0900

    change5-2

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -1,12 +1,13 @@
 // テキスト「関数を作る(1) 引数のある関数」
 // 練習問題：星を描く関数を改造して正N角形を描画する関数を作ってみよう
 function setup(){
-  createCanvas(300, 100);
+  createCanvas(400, 100);
   background(200);
   fill(0);
-  crossmark(10, 10, 90, 90);
+  crossmark(10, 10, 90, 90);　　//自分で作る　何回も書くときに役立つ
   ngmark(150, 50, 80);
   star(250, 50, 40);
+  regularPolygon(10, 350, 50, 50);
 }
 
 function crossmark(x1, y1, x2, y2){
@@ -27,7 +28,18 @@ function ngmark(cx, cy, r){
 function star(cx, cy, r){
   beginShape();
   for(var i = 0; i < 5; i++){
-    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;  //2/5の2の部分が、2個先の点を指している。1/5にすれば正五角形になる
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
+}
+
+function regularPolygon(n, cx, cy, r){
+  beginShape();
+  for(var i = 0; i < n; i++){
+    let theta = TWO_PI * i * 1 / n - HALF_PI;  //2/5の2の部分が、2個先の点を指している。1/5にすれば正五角形になる
     let x = cx + cos(theta) * r;
     let y = cy + sin(theta) * r;
     vertex(x,y);
