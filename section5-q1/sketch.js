bb5888864be61ad266228b4ba5b99c700587f8a5
Yoshiya04 <major.yoshiya@gmail.com>
2021-11-16 10:00:28 +0900

    change5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -10,9 +10,26 @@ function setup(){
 function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  let p = 100;
+  fill(255,0,155);
+  ellipse(200, h+p, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, p+50, h + p);
 }
+
+function draw(){
+  fill(255,0,155);
+  noStroke();
+  triangle(300,200,320,180,360,220);
+}
+
+
+// function balloon(t){
+//   let w = textWidth(t);
+//   let h = textAscent() + textDescent();
+//   let p = 2;
+//   fill(0);
+//   ellipse(0, 0, w + p * 2, h + p * 2);
+//   fill(255);
+//   text(t, p, h + p);
+// }
