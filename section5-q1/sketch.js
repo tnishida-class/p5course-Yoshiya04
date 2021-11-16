46943544e96c08dc3a38c6663495b270debb48ce
Yoshiya04 <major.yoshiya@gmail.com>
2021-11-16 11:54:22 +0900

    change5-q1(2)

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -3,25 +3,44 @@
 // 吹き出しにしっぽを付けてみよう
 function setup(){
   createCanvas(400, 400);
-  background(255);
-  balloon("I love keyakizaka46");
+  // background(255);
+  // balloon("I love keyakizaka46");
 }
 
-function balloon(t){
-  let w = textWidth(t);
-  let h = textAscent() + textDescent();
-  let p = 100;
-  fill(255,0,155);
-  ellipse(200, h+p, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p+50, h + p);
-}
+// function balloon(t){
+//   let w = textWidth(t);
+//   let h = textAscent() + textDescent();
+//   let p = 100;
+//   fill(255,0,155);
+//   ellipse(200, h+p, w + p * 2, h + p * 2);
+//   fill(255);
+//   text(t, p+50, h + p);
+// }
 
 function draw(){
-  fill(255,0,155);
-  noStroke();
-  triangle(300,200,320,180,360,220);
+  background(255);
+  balloon("I love keyakizaka46",mouseX,mouseY);
 }
+
+function balloon(t,x,y){
+  push(); //push popいれる方がいい　（色変える時とかに必要）
+  let w = textWidth(t);
+  let h = textAscent() + textDescent();
+  let p = 2;
+  fill(255,0,155);
+  rect(x, y, w + p * 2, h + p * 2);
+  noStroke();
+  triangle(x,y+h,x,y+h+10,x+10,y+h);
+  fill(255);
+  text(t, x+p , y + h + p); //xとyに余白分を足している
+  pop();
+}
+
+// function draw(){
+//   fill(255,0,155);
+//   noStroke();
+//   triangle(300,200,320,180,360,220);
+// }
 
 
 // function balloon(t){
